59d6a7724c9e2656e98ebd76394696afb42038d7
SimiGold1 <ajisafebabygold@gmail.com>
2023-10-13 13:35:47 +0100

    updating changes

--- src/Components/Greeting.jsx
+++ src/Components/Greeting.jsx
@@ -6,7 +6,10 @@ const GreetingComponent = () => {
   return (
       <div>
         {firstName ? (
+          <>
           <p>Hello, {firstName}!</p>
+          <img src="../Fendi.jpeg" alt="Profile" />
+          </>
         ) : (
           <p>Hello, there!</p>
         )}
